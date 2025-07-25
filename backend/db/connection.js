e203fb00520acfdc79986b0693284c8bfd788596
mainak531 <mainakchanda531@gmail.com>
2025-07-25 19:34:25 +0530

    Update connection.js

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,29 +1,21 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const DB_NAME = process.env.DB_NAME || "kollab";
-const DB_USER = process.env.DB_USER || "kollabadmin";
-const DB_PASS = process.env.DB_PASSWORD || "kollabpass";
-const DB_HOST = process.env.DB_HOST || "localhost";
-const useSSL = process.env.DB_SSL === "true";
-
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: DB_HOST,
-  dialect: "postgres",
-  dialectOptions: useSSL
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      }
-    : {},
-  logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  },
+  logging: false
 });
 
-db.authenticate()
+sequelize.authenticate()
   .then(() => console.log("✅ Database connected"))
   .catch((err) => console.error("❌ Error creating DB -", err));
 
-module.exports = db;
+module.exports = sequelize;
+
 
 
